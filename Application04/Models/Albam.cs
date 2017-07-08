514a38dc650c43182e5093948a51d11166b989aa
hasitha <hasithamadusanka90@gmail.com>
2017-07-08 14:16:57 +0530

    app 05 initial commit

---
+++ Application04/Models/Albam.cs
@@ -0,0 +1,19 @@
+﻿using Application04.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Application04.Models
+{
+    public class Albam
+    {
+        public int AlbamID { set; get; }
+
+        [Required()]
+        [StringLength(100,MinimumLength =2)]
+        public String Title { get; set; }
+        public Artist MyProperty { get; set; }
+    }
+}
---
+++ Application05/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Application05.Startup))]
+namespace Application05
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
